cd3365d78b9f35427317e944a995515b84bdff35
Daniel <daniel.r.lazarov@gmail.com>
2015-06-18 02:05:59 +0300

    fix

--- test_me-windows_phone_ui/ServerAPI/RequestHandler.cs
+++ test_me-windows_phone_ui/ServerAPI/RequestHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-<<<<<<< HEAD
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,35 +11,21 @@ using System.Windows.Shapes;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-=======
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
->>>>>>> origin/master
 using System.IO;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Schema;
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> origin/master
 namespace ServerAPI
 {
     public class RequestHandler
     {
         public event EventHandler<ResponseArrivedEventArgs> ResponseArrived;
-<<<<<<< HEAD
         private byte[] data;
         private string dataString;
         
-=======
-
->>>>>>> origin/master
         public void SendRequest(Dictionary<string, string> requestParams)
         {
 
@@ -51,7 +36,6 @@ namespace ServerAPI
                 {
                     if (kvp.Key != "action")
                     {
-<<<<<<< HEAD
                         dataToSend += "&" + kvp.Key + "=" + Uri.EscapeDataString(kvp.Value);
                     }
                 }
@@ -65,19 +49,6 @@ namespace ServerAPI
 
                 // Start the asynchronous operation to send the request data
                 request.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), request);
-=======
-                        dataToSend += "&" + kvp.Key + "=" + kvp.Value;
-                    }
-                }
-                UTF8Encoding encoding = new UTF8Encoding();
-                byte[] data = encoding.GetBytes(dataToSend);
-
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://78.90.52.246:88");
-                request.Method = "POST";
-                request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
-
-                request.BeginGetResponse(new AsyncCallback(ReadWebRequestCallback), request);
->>>>>>> origin/master
             }
             catch (Exception ex)
             {
@@ -92,7 +63,6 @@ namespace ServerAPI
             }
         }
 
-<<<<<<< HEAD
         private void GetRequestStreamCallback(IAsyncResult asynchronousResult)
         {
             HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
@@ -106,8 +76,6 @@ namespace ServerAPI
             request.BeginGetResponse(new AsyncCallback(ReadWebRequestCallback), request);
         }
         
-=======
->>>>>>> origin/master
         private void ReadWebRequestCallback(IAsyncResult callbackResult)
         {
             HttpWebRequest myRequest = (HttpWebRequest)callbackResult.AsyncState;
@@ -119,7 +87,6 @@ namespace ServerAPI
                 JsonSchema schema = JsonSchema.Parse(results);
                 ServerResponseData respData = JsonConvert.DeserializeObject<ServerResponseData>(results);
                 respData.resultString = JsonConvert.SerializeObject(respData.result);
-<<<<<<< HEAD
                 
                 ResponseArrivedEventArgs resp = new ResponseArrivedEventArgs();
                 resp.response = respData;
@@ -130,18 +97,6 @@ namespace ServerAPI
         }
 
         public virtual void OnResponseArrived(ResponseArrivedEventArgs e)
-=======
-
-                ResponseArrivedEventArgs resp = new ResponseArrivedEventArgs();
-                resp.response = respData;
-
-                this.OnResponseArrived(resp);
-            }
-            myResponse.Dispose();
-        }
-
-        protected virtual void OnResponseArrived(ResponseArrivedEventArgs e)
->>>>>>> origin/master
         {
             EventHandler<ResponseArrivedEventArgs> handler = ResponseArrived;
             if (handler != null)
@@ -161,10 +116,7 @@ namespace ServerAPI
         public string status { get; set; }
         public string message { get; set; }
         public object result { get; set; }
-<<<<<<< HEAD
         public string error_code { get; set; }
-=======
->>>>>>> origin/master
         public string resultString { get; set; }
     }
 }
--- test_me-windows_phone_ui/test_me-windows_phone_ui/pages/loginPage.xaml.cs
+++ test_me-windows_phone_ui/test_me-windows_phone_ui/pages/loginPage.xaml.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,31 +10,10 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
-=======
-﻿using test_me_windows_phone_ui.Common;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Graphics.Display;
-using Windows.UI.ViewManagement;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.UI.Popups;
->>>>>>> origin/master
 
 using Newtonsoft.Json;
 
 using ServerAPI;
-<<<<<<< HEAD
 using System.IO.IsolatedStorage;
 
 
@@ -60,109 +38,6 @@ namespace test_me_windows_phone_ui.pages
         {
             if (txtUsername.Text == "")
                 txtUsername.Text = "Username";
-=======
-using Windows.Storage;
-
-// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
-
-namespace test_me_windows_phone_ui.pages
-{
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class loginPage : Page
-    {
-        private NavigationHelper navigationHelper;
-        private ObservableDictionary defaultViewModel = new ObservableDictionary();
-
-        public loginPage()
-        {
-            this.InitializeComponent();
-
-            this.navigationHelper = new NavigationHelper(this);
-            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
-            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
-        }
-
-        /// <summary>
-        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
-        /// </summary>
-        public NavigationHelper NavigationHelper
-        {
-            get { return this.navigationHelper; }
-        }
-
-        /// <summary>
-        /// Gets the view model for this <see cref="Page"/>.
-        /// This can be changed to a strongly typed view model.
-        /// </summary>
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return this.defaultViewModel; }
-        }
-
-        /// <summary>
-        /// Populates the page with content passed during navigation.  Any saved state is also
-        /// provided when recreating a page from a prior session.
-        /// </summary>
-        /// <param name="sender">
-        /// The source of the event; typically <see cref="NavigationHelper"/>
-        /// </param>
-        /// <param name="e">Event data that provides both the navigation parameter passed to
-        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
-        /// a dictionary of state preserved by this page during an earlier
-        /// session.  The state will be null the first time a page is visited.</param>
-        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
-        {
-        }
-
-        /// <summary>
-        /// Preserves state associated with this page in case the application is suspended or the
-        /// page is discarded from the navigation cache.  Values must conform to the serialization
-        /// requirements of <see cref="SuspensionManager.SessionState"/>.
-        /// </summary>
-        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
-        /// <param name="e">Event data that provides an empty dictionary to be populated with
-        /// serializable state.</param>
-        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
-        {
-        }
-
-        #region NavigationHelper registration
-
-        /// <summary>
-        /// The methods provided in this section are simply used to allow
-        /// NavigationHelper to respond to the page's navigation methods.
-        /// <para>
-        /// Page specific logic should be placed in event handlers for the  
-        /// <see cref="NavigationHelper.LoadState"/>
-        /// and <see cref="NavigationHelper.SaveState"/>.
-        /// The navigation parameter is available in the LoadState method 
-        /// in addition to page state preserved during an earlier session.
-        /// </para>
-        /// </summary>
-        /// <param name="e">Provides data for navigation methods and event
-        /// handlers that cannot cancel the navigation request.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            this.navigationHelper.OnNavigatedTo(e);
-        }
-
-        protected override void OnNavigatedFrom(NavigationEventArgs e)
-        {
-            this.navigationHelper.OnNavigatedFrom(e);
-        }
-
-        private void txtEmail_GotFocus(object sender, RoutedEventArgs e)
-        {
-            txtEmail.Text = "";
-        }
-
-        private void txtEmail_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (txtEmail.Text == "")
-                txtEmail.Text = "Email";
->>>>>>> origin/master
         }
 
         private void txtPassword_LostFocus(object sender, RoutedEventArgs e)
@@ -180,19 +55,14 @@ namespace test_me_windows_phone_ui.pages
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-<<<<<<< HEAD
             this.NavigationService.Navigate(
                       new Uri("//pages/registerPage.xaml", UriKind.Relative));
-=======
-            //TODO Navigate to register Page
->>>>>>> origin/master
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             RequestHandler rq = new RequestHandler();
             rq.ResponseArrived += new EventHandler<ResponseArrivedEventArgs>(rq_ResponseArrived);
-<<<<<<< HEAD
             Dictionary<string,string> reqData = new Dictionary<string,string>();
 
             if (String.IsNullOrEmpty(txtUsername.Text) || txtUsername.Text == "Username")
@@ -241,36 +111,6 @@ namespace test_me_windows_phone_ui.pages
             }      
         }
     }
-=======
-            Dictionary<string, string> reqData = new Dictionary<string, string>();
-
-            reqData.Add("action", "login");
-            reqData.Add("email", txtEmail.Text);
-            reqData.Add("password", txtPassword.Password);
-
-            rq.SendRequest(reqData);
-        }
-
-        private async void rq_ResponseArrived(object sender, ResponseArrivedEventArgs e)
-        {
-            MessageDialog msgbox;
-            ServerResponseData srd = e.response;
-            ApplicationDataContainer settings = ApplicationData.Current.LocalSettings;
-            if (srd.result != null && srd.status == "OK")
-            {
-                LoginData respData = JsonConvert.DeserializeObject<LoginData>(srd.resultString);
-                msgbox = new MessageDialog(respData.first_name);
-                await msgbox.ShowAsync();
-                //TODO Navigate to profile page
-            }
-            else
-            {
-                msgbox = new MessageDialog(srd.message);
-            }         
-        }
-    }
-
->>>>>>> origin/master
     public struct LoginData
     {
         public int rank_id;
@@ -281,12 +121,4 @@ namespace test_me_windows_phone_ui.pages
         public string rank__name;
         public string email;
     }
-<<<<<<< HEAD
 }
-=======
-
-
-    #endregion
-
-}
->>>>>>> origin/master
